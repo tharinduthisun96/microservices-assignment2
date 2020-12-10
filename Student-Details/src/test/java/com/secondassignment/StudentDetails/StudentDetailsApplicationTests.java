48390481c0660da8fddd8e3cd42c9076d389b719
Tharindu Akalaka <tharindu.thisun123@gmail.com>
2020-12-10 15:32:31 +0530

    first commit

---
+++ Student-Details/src/test/java/com/secondassignment/StudentDetails/StudentDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondassignment.StudentDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
